da91cac8989600b1e1356fdc49547adfbf41427b
beyazite <enisbyzt@gmail.com>
2023-12-29 17:27:37 +0300

    header and footer are completed, footer links work

--- src/pages/shared components/footer.js
+++ src/pages/shared components/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function footer() {
   return (
-   <footer className="bg-success container fixed-bottom">
+   <footer className="bg-success container fixed-bottom rounded-top">
      {/* <div className='contact-info'>
       <div className='social-media d-flex justify-content-center align-items-center'>
         <p>Social Media</p>
@@ -15,11 +15,11 @@ function footer() {
         </ul>
       </div>
      </div> */}
-     <p className='text-center'>Contact Me</p>
-     <ul className="list-group list-group-horizontal mx-auto mb-2" style={{width: "150px"}}>
-  <li className="list-group-item"><i class="bi bi-linkedin"></i></li>
-  <li className="list-group-item"><i class="bi bi-instagram"></i></li>
-  <li className="list-group-item"><i class="bi bi-envelope-at"></i></li>
+     <p className='text-center mb-1'>Contact Me</p>
+     <ul className="list-group list-group-horizontal mx-auto mb-2 " style={{width: "150px"}}>
+  <a href="https://linkedin.com/in/nino-azumi-fajarini-483740200/" target='_blank' className='rounded-start'><li className="list-group-item"><i class="bi bi-linkedin"></i></li></a>
+  <a href='https://instagram.com/savourybynazumi/' target='_blank'><li className="list-group-item"><i class="bi bi-instagram"></i></li></a>
+  <a href='mailto:ohanazumi@gmail.com' className='rounded-end'><li className="list-group-item"><i class="bi bi-envelope-at"></i></li></a>
 </ul>
 
    </footer>
--- src/pages/shared components/header.js
+++ src/pages/shared components/header.js
@@ -3,18 +3,21 @@ import {Link} from "react-router-dom";
 function header() {
   return (
     
-    <nav className="navbar navbar-expand-sm bg-body-tertiary">
-      <div className="container-fluid">
-      <div className='d-flex-md mx-auto' style={{width: "160px"}}>
+    <nav className="navbar navbar-expand-sm bg-body-tertiary fixed-top">
+      <div className="container-fluid position-relative">
+      <div className='ps-md-5'  >
        <Link className="navbar-brand" to="/">A Plate of Love - <small>Azumi Fajarini</small></Link>
        </div>
-       
+       {/* <div>
+        <img src={"images/tree-branch.png"} alt="A tree branch with crimson flowers on it" width="100px"/>
+       </div> */}
        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className='d-flex justify-content-end'>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+
+          <div className='ms-auto'>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 pe-5">
             <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="/">Home</Link>
             </li>
@@ -25,9 +28,9 @@ function header() {
               <Link className="nav-link" to="/gallery">Gallery</Link>
             </li>
           </ul>
-          
+          </div>
         </div>
-       </div>
+       
        
        </div>
        
