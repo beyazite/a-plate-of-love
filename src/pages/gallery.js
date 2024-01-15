a6bfcda7d2f0a4a6fa5ae1a8c8d6155acc36927a
beyazite <enisbyzt@gmail.com>
2024-01-15 16:45:01 +0300

    react-photo-view package and photos added for gallery, check for responsive

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -2,8 +2,12 @@ import React from 'react'
 import { Helmet } from 'react-helmet-async'
 import Header from "./components/shared components/header";
 import Footer from "./components/shared components/footer";
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+
 
 function gallery() {
+
+  const images = ["/assets/azumi-basket.jpeg","assets/food1.jpeg","assets/class.jpeg","assets/food2.jpeg","/assets/azumi-friends.jpeg","assets/food.jpeg","/assets/azumi-graduated.jpeg","assets/food3.jpeg","assets/tree-branch.png","assets/food4.jpeg","assets/applause.jpeg","assets/food5.jpeg","assets/grad.jpeg","assets/grad1.jpeg"];
   return (
     <div>
       <Helmet>
@@ -12,6 +16,15 @@ function gallery() {
       </Helmet>
       <Header/>
       <div style={{position: "absolute", top: "56px"}}>gallery</div>
+      <PhotoProvider >
+      <div className="foo mt-5">
+        {images.map((item, index) => (
+          <PhotoView key={index} src={item}>
+            <img src={item} alt=""  className='m-1' />
+          </PhotoView>
+        ))}
+      </div>
+    </PhotoProvider>
       <Footer/></div>
   )
 }
